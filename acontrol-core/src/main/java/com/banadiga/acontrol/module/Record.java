5c40dfd06992a6f10ad0f676c4a2feaabaeb4c0e
Ihor Banadiga <banadiga@gmail.com>
2015-11-30 21:51:25 +0200

    Create multi module project

---
+++ acontrol-core/src/main/java/com/banadiga/acontrol/module/Record.java
@@ -0,0 +1,13 @@
+package com.banadiga.acontrol.module;
+
+import java.util.UUID;
+
+import lombok.Data;
+import lombok.experimental.Builder;
+
+@Data
+@Builder
+public class Record {
+
+  public UUID id;
+}
---
+++ data-import/src/main/java/com/banadiga/acontrol/dataimport/command/DataImportCommandLineRunner.java
@@ -0,0 +1,20 @@
+package com.banadiga.acontrol.dataimport.command;
+
+import com.banadiga.acontrol.module.Record;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import java.util.UUID;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Component
+@Slf4j
+public class DataImportCommandLineRunner implements CommandLineRunner {
+
+  @Override
+  public void run(String... args) throws Exception {
+    log.info("DataImportCommandLineRunner Record: {}", Record.builder().id(UUID.randomUUID()).build());
+  }
+}
