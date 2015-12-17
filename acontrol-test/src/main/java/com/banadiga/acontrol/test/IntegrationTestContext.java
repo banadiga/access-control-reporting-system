100dd0239a9cf40ccda343798893a41c7ab731dd
Ihor Banadiga <banadiga@gmail.com>
2015-12-17 20:25:26 +0200

    dependency-management added

--- acontrol-test/src/main/java/com/banadiga/acontrol/test/IntegrationTestContext.java
+++ acontrol-test/src/main/java/com/banadiga/acontrol/test/IntegrationTestContext.java
@@ -9,9 +9,9 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-@Target({ElementType.CONSTRUCTOR, ElementType.FIELD, ElementType.METHOD, ElementType.ANNOTATION_TYPE, ElementType.TYPE})
-@Retention(RetentionPolicy.RUNTIME)
 @Documented
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.CONSTRUCTOR, ElementType.FIELD, ElementType.METHOD, ElementType.ANNOTATION_TYPE, ElementType.TYPE})
 
 @ContextConfiguration(
     loader = AnnotationConfigContextLoader.class,
